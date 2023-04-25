3ff58c9acdf8d1203b1687788715e57c5bd2f19a
lalCorr <lal.corrigan@gmail.com>
2023-04-25 16:11:55 +0000

    Add programme details functionality

--- programmes/views.py
+++ programmes/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Programme
 
 # Create your views here.
 
 
 def all_programmes(request):
-    """ A view to show all programmes """
+    """ A view to show individual programme details """
 
     programmes = Programme.objects.all()
 
@@ -14,3 +14,15 @@ def all_programmes(request):
     }
 
     return render(request, 'programmes/programmes.html', context)
+
+
+def programme_detail(request, programme_id):
+    """ A view to show all programmes """
+
+    programme = get_object_or_404(Programme, pk=programme_id)
+
+    context = {
+        'programme': programme,
+    }
+
+    return render(request, 'programmes/programme_detail.html', context)
