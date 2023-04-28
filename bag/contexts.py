744de3480e9086f9188a6dcbfdac6d603eb79f14
lalCorr <lal.corrigan@gmail.com>
2023-04-28 11:29:07 +0000

    Update shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -15,7 +15,7 @@ def bag_contents(request):
         total += quantity * product.price
         product_count += quantity
         bag_items.append({
-            'item_id' : item_id,
+            'item_id': item_id,
             'quantity': quantity,
             'product': product,
         })
