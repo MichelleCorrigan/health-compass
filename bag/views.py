751109577c3ffbb0676baa65334430f826018752
lalCorr <lal.corrigan@gmail.com>
2023-05-30 11:58:38 +0000

    Add +/- quantity buttons

--- bag/views.py
+++ bag/views.py
@@ -13,48 +13,57 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    # to add more than one of same programme/product #
-    # if item_id in list(bag.keys()):
-    #     bag[item_id] += quantity
-    # else:
-    bag[item_id] = quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
-# def remove_from_bag(request, item_id):
-#     """Remove item from bag"""
-
-#     quantity = int(request.POST.get('quantity'))
-#     bag = request.session.get('bag', {})
-
-#     if quantity > 0:
-#         bag[item_id] = quantity
-#     else:
-#         bag.pop[item_id]
-
-#     request.session['bag'] = bag
-#     return redirect(reverse('view_bag'))
-
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
-
+    """Remove item from bag"""
     try:
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
+        quantity = str(request.POST.get('quantity'))
         bag = request.session.get('bag', {})
-
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-        else:
-            bag.pop(item_id)
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-
     except Exception as e:
-        return HttpResponse(status=500)
+        return redirect(reverse('view_bag'))
+
+    bag[item_id] = quantity
+    del bag[item_id]
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
+    
+
+
+    # if quantity == 1:
+        
+    # else:
+        
+
+    # request.session['bag'] = bag
+    # return redirect(reverse('view_bag'))
+
+# def remove_from_bag(request, item_id):
+#     """Remove the item from the shopping bag"""
+
+#     try:
+#         size = None
+#         if 'product_size' in request.POST:
+#             size = request.POST['product_size']
+#         bag = request.session.get('bag', {})
+
+#         if size:
+#             del bag[item_id]['items_by_size'][size]
+#             if not bag[item_id]['items_by_size']:
+#                 bag.pop(item_id)
+#         else:
+#             bag.pop(item_id)
+
+#         request.session['bag'] = bag
+#         return HttpResponse(status=200)
+
+#     except Exception as e:
+#         return HttpResponse(status=500)
