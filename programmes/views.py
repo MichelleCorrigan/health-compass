0c91d40a1c280024a95bed33885410790d080e1b
lalCorr <lal.corrigan@gmail.com>
2023-07-04 13:53:58 +0000

    Finish add-programme functionality for admin

--- programmes/views.py
+++ programmes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
 from .models import Programme
 from .forms import ProgrammeForm
 
@@ -29,7 +31,17 @@ def programme_detail(request, programme_id):
 
 def add_programme(request):
     """ Add a programme to the site """
-    form = ProgrammeForm()
+    if request.method == 'POST':
+        form = ProgrammeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added programme/service!')
+            return redirect(reverse('add_programme'))
+        else:
+            messages.error(request, 'Failed to add programme/service. Please ensure the form is valid.')
+    else:
+        form = ProgrammeForm()
+        
     template = 'programmes/add_programme.html'
     context = {
         'form': form,
