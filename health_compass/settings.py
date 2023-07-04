0a54081f64d0992b29d8dfb4be6cdc8cce5cbe0c
lalCorr <lal.corrigan@gmail.com>
2023-07-04 18:30:50 +0000

    Update stripe-wh name in settings

--- health_compass/settings.py
+++ health_compass/settings.py
@@ -212,5 +212,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_KEY = os.getenv('STRIPE_WH_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'healthcompass@example.com'
