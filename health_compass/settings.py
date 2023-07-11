4fccb3bc54d82657d9d569cdc8e77a5d5edaf8d8
lalCorr <lal.corrigan@gmail.com>
2023-07-11 17:34:49 +0000

    Shopping bag refactor

--- health_compass/settings.py
+++ health_compass/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['health-compass.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*', 'health-compass.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -207,7 +207,7 @@ if 'USE_AWS' in os.environ:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
-STRIPE_CURRENCY = 'usd'
+STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
