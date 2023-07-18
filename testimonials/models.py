5f9e3da8cc8988ef5bb4e24514621603ccf52d10
lalCorr <lal.corrigan@gmail.com>
2023-07-18 18:33:35 +0000

    Add testimonial model

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Testimonial(models.Model):
+    review = models.TextField()
+    name = models.CharField(max_length=50)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.review
