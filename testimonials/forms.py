29d96e8a4fee3ae7233a980b2856d6da8bb8e674
lalCorr <lal.corrigan@gmail.com>
2023-07-19 16:53:53 +0000

    Add Testimonial form for admin

---
+++ testimonials/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Testimonial
+
+
+class TestimonialForm(form.ModelForm):
+
+    class Meta:
+        model = Testimonial
+        fields = '__all__'
+
+
+def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    for field_name, field in self.fields.items():
+        field.widget.attrs['class'] = 'border-black rounded-0'
